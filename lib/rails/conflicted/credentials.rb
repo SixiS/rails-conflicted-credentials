beb4808eae32e885506b0b45c62943d57999dba4
Matthew Hirst <hirst.mat@gmail.com>
2025-06-23 22:43:20 +0300

    Initial commit.

---
+++ lib/rails/conflicted/credentials.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "credentials/version"
+
+module Rails
+  module Conflicted
+    module Credentials
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rails/conflicted/credentials_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rails::Conflicted::Credentials do
+  it "has a version number" do
+    expect(Rails::Conflicted::Credentials::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
